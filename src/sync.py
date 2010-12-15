43db66f09cd45fda1c0a82e941cbb6c7ef456160
Carlos Aguilar <caguilar@dwdandsolutions.com>
2010-12-15 10:53:27 -0600

    Agregado un bloque for para sincronizar las carpetas que se ignoran la primera vez

--- src/sync.py
+++ src/sync.py
@@ -35,7 +35,7 @@ args = " --syncinternaldates --useheader 'Message-Id' --host1 %s --port1 993 --u
         --nofoldersizes --skipsize --allowsizemismatch --exclude 'Drafts|Trash|Spam|Sent'"
         
 args2 = " --syncinternaldates --useheader 'Message-Id' --host1 %s --port1 993 --user1 %s --password1 %s --ssl1 \
-        --host2 %s --port2 993 --user2 %s --password2 %s --ssl2 --noauthmd5 --split1 200 --split2 200 --nofoldersizes --skipsize --allowsizemismatch \
+        --host2 %s --port2 993 --user2 %s --password2 %s --ssl2 --noauthmd5 --authmech1 LOGIN --authmech2 LOGIN --split1 200 --split2 200 --nofoldersizes --skipsize --allowsizemismatch \
         --folder 'Inbox/Sent' --prefix2 '[Gmail]/' --regextrans2 's/Inbox\/Sent/Sent Mail/' \
         --folder \"Inbox/Spam\" --prefix2 '[Gmail]/' --regextrans2 's/Inbox\/Spam/Spam/' \
         --folder \"Inbox/Trash\" --prefix2 '[Gmail]/' --regextrans2 's/Inbox\/Trash/Trash/' \
@@ -48,6 +48,24 @@ host2 = "imap.googlemail.com"
 # Leemos el archivo de la lista de usuarios y ejecutamos imapsync por cada uno de ellos
 for line in userList.readlines():
     imapsync = Popen(command + args % (host1, line.split(" ")[0], line.split(" ")[1],
+        host2, line.split(" ")[2], line.split(" ")[3].rstrip('\n')), stdout=PIPE, shell=True)
+    print '%s --- Sincronizando la cuenta "%s"...' % (strftime("%d-%m-%Y %H:%M:%S"), (line.split(" ")[0]))
+
+    # Si el log está habilitado, lo escribimos en un archivo
+    if log:
+        stdout = imapsync.stdout.read()
+        logFile = open("imapsync.log", "a")
+        logFile.write('%s --- Sincronizando la cuenta "%s"... \n\n %s \n\n' % (strftime("%d-%m-%Y %H:%M:%S"),
+            (line.split(" ")[0]), stdout))
+        logFile.close()
+
+    # De lo contrario, lo mostramos por pantalla
+    else:
+        print imapsync.stdout.read()
+        
+# Sincronizamos las carpetas que se excluyeron en el paso anterior
+for line in userList.readlines():
+    imapsync = Popen(command + args2 % (host1, line.split(" ")[0], line.split(" ")[1],
         host2, line.split(" ")[2], line.split(" ")[3].rstrip('\n')), stdout=PIPE, shell=True)
     print '%s --- Sincronizando la cuenta "%s"...' % (strftime("%d-%m-%Y %H:%M:%S"), (line.split(" ")[0]))
 
